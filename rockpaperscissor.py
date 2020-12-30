377e55dcd260fe93c75783c519b5013fa76f5932
Gowtham <gowtham758550@gmail.com>
2020-12-30 20:13:19 +0530

    Update rockpaperscissor.py

--- rockpaperscissor.py
+++ rockpaperscissor.py
@@ -1,92 +1,105 @@
-import random
-import time
-# Games starts here
-print("^__^~~~~~~~~~~~~~~~~~~~~ Rock Paper Scissor ~~~~~~~~~~~~~~~~~~~~^__^")
+from random import choice
+from time import sleep
+
+
+#Games starts here
+print("Rock Paper Scissor".center(50, "~"))
 user = input("Enter player name: ")
 print("Let's start the game....")
 
-# Get target score
-try:
-    n = input("Enter your target score: ")
-    n = int(n)
-except:
-    print("Use only numbers")
-    while not n.isnumeric():
-        n = input("Enter your target score: ")
-    n = int(n)
 
-userscore = 0
-compscore = 0
+def game():				
+	#Get target score
+	while(True):
+	    try:
+                n = int(input("Enter target score : "))
+                break
+	    except ValueError:
+	        print("Enter numbers only")
+	userscore = 0
+	compscore = 0
+	#Compares rock , paper and scissor
+	while True:
+ 	    if userscore == n or compscore == n:
+                break
+ 	    print("\n Rock => 1\t Paper => 2\t Scissor => 3")
+ 	    choice_list = [1, 2, 3]
+ 	    while(True):
+                try:
+                    choice1 = int(input(f"{user} turn : "))
+                    if choice1 in choice_list:
+                    	break
+                    else:
+                    	print("Enter 1 or 2 or 3 only")
+                except ValueError:
+                	print("Enter 1 or 2 or 3 only")
+ 	    
+ 	    choice2 = choice(choice_list)
+ 	    if choice1 == choice2:
+ 	    	print("Draw!!!")
+ 	    elif choice1 == 1:
+ 	              		if choice2 == 2:
+ 	              			print("Paper beats rock\nComputer Wins!!!")
+ 	              			compscore += 1
+ 	              		else:
+ 	              			print(f"Rock beats scissor\n{user} Wins!!!")
+ 	              			userscore += 1
+ 	    elif choice1 == 2:  
+                	if choice2 == 3:
+                		print("Scissor beats paper\nComputer Wins!!!")
+                		compscore += 1
+                	else:
+                		print(f"Rock beats scissor\n{user} Wins!!!")
+                		userscore += 1
+ 	    else:
+		              		if choice2 == 2:
+		              			print("Scissor beats paper\n{user} Wins!!!")
+		              			userscore += 1
+		              		else:
+		              			print("Rock beats scissor\nComputer Wins!!!")
+		              			compscore += 1
+                          
+	# Compares two scores
+	if userscore == compscore:
+ 	   r = "You and computer gets draw!!!"
+ 	   result = 0
+	elif userscore > compscore:
+ 	   r = f"{user} smashed the computer"
+ 	   result = 1
+	else:
+ 	   r = "Computer beats you"
+ 	   result = -1
 
-# Compares rock , paper and scissor
-while True:
-    if ((userscore >= n) and (compscore <= n)) or ((userscore <= n) and (compscore >= n)):
-        break
-    print("\n Rock => 1\t Paper => 2\t Scissor => 3")
-    try:
-        choice1 = input("\n" + user + "'s Turn: ")
-        choice1 = int(choice1)
-    except:
-        print("Use only numbers")
-        while not choice1.isnumeric():
-            print("\n___Enter 1 or 2 or 3___")
-            choice1 = input(user + "'s Turn: ")
-        choice1 = int(choice1)
-    choice2 = random.choice((1, 2, 3))
-    if choice1 == choice2:
-        print("Draw!!!")
-    elif choice1 == 1:  # rock
-        if choice2 == 2:
-            print("Computer Wins!!!")
-            compscore += 1
-        if choice2 == 3:
-            print(user, "Wins!!!")
-            userscore += 1
-    elif choice1 == 2:  # paper
-        if choice2 == 3:
-            print("Computer Wins!!!")
-            compscore += 1
-        if choice2 == 1:
-            print(user, "Wins!!!")
-            userscore += 1
-    elif choice1 == 3:  # scissor
-        if choice2 == 2:
-            print(user, "Wins!!!")
-            userscore += 1
-        if choice2 == 1:
-            print("Computer Wins!!!")
-            compscore += 1
-    else:
-        print("Use only (1 or 2 or 3) to beat the computer")
-# Compares two scores
-if userscore == compscore:
-    r = "\nYou and computer gets draw!!!"
-elif userscore > compscore:
-    r = user + " smashed the computer"
-elif userscore < compscore:
-    r = "Computer beats you"
-print("""\n/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+	print(f"""\n/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
 /                                                                 /
-                 """ + r + """                               
+{r.center(67)}                               
 /                                                                 /
 /\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/""")
-print("""\n        Score Board           
-    """+f"{user}'s score: {userscore}"+"""
-    """+f"Computer's score: {compscore}")
+	print(f"Score Board\n{user}'s score: {userscore}\nComputer's score: {compscore}")
 
-# some conversation
-if r == "Computer beats you":
-    time.sleep(0.5)
-    print("\nHa ha ha ha ha.....")
-    time.sleep(2)
-    print("Don't try to beat me")
-    time.sleep(2)
-    print("Humans are not more powerful than me ^_^")
-    time.sleep(1)
-elif r == user + " smashed the computer":
-    time.sleep(1)
-    print("\nComputer >> Not all time like this")
-    time.sleep(2)
-    print("\tI'm just kidding")
-ex = input("\nPress Enter to exit")
-exit(1)
+	# some conversation
+	if result == -1:
+ 	    sleep(.5)
+ 	    print("\nHa ha ha ha ha.....")
+ 	    sleep(.5)
+ 	    print("Don't try to beat me")
+ 	    sleep(.5)
+ 	    print("Humans are not more powerful than me ^_^")
+	elif result == 1:
+ 	   sleep(.5)
+ 	   print("\nComputer >> Not all time like this")
+ 	   sleep(.5)
+ 	   print("\tI'm just kidding")
+	else:
+	    sleep(.5)
+	    print("\nYou have enough power to fight with me")
+
+def main():
+    while(True):
+     game()
+     if not input("Want to play again (y or n) : ") == "y":
+     	break
+			
+if __name__ == "__main__":
+    main()
+
